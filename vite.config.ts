33224444e1f73c033c81066c1c237347a174b5d7
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2025-11-29 16:40:16 +0000

    checkpoint

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,54 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        // {
+        //   src: 'src/test.ts', // Source path
+        //   dest: '', // Destination folder inside dist
+        // },
+        // {
+        //   src: 'index.html', // Source path
+        //   dest: '' // Destination folder is dist itself
+        // }
+      ],
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       content: 'src/test.js', // Build `main.js` as the content script
+  //       // background: 'src/test.js', // Add `background.js` as another entry point
+  //     },
+  //     output: {
+  //       entryFileNames: (chunkInfo) => {
+  //         // Dynamically name files based on their entry point
+  //         if (chunkInfo.name === 'content') {
+  //           return 'main.js'; // Output content script as `main.js`
+  //         }
+  //         if (chunkInfo.name === 'test') {
+  //           return 'test.js'; // Output background script into `background/`
+  //         }
+  //         return '[name].js'; // Default naming for other files
+  //       },
+  //     },
+  //     // commonjsOptions: {
+  //     //   include: [/node_modules/], // Include dependencies in the bundle
+  //     //   format: 'iife',
+  //     // },
+  //     external: []
+  //   },
+  // },
 })
