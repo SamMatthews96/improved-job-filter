23f4a76cfeeda20b1bdfb8a71321cc140f3c0aa7
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-01-13 13:54:45 +0000

    removed shadow dom

--- src/script.ts
+++ src/script.ts
@@ -2,33 +2,15 @@ import { createApp } from 'vue'
 import WebsiteConfig from '@/components/WebsiteConfig.vue'
 import Filter from '@/utils/filter'
 
-import cssUrl from './assets/style.scss?url'
-import './assets/style.css'
-
 const id = 'improved-job-filter-overlay'
 
 function createOverlay(): void {
-    if (document.querySelector(`#${id}`)) {
-        return
-    }
+    if (document.querySelector(`#${id}`)) return
+
     const host = document.createElement('div')
     host.id = id
     document.body.appendChild(host)
-
-    const shadow = host.attachShadow({ mode: "open" });
-
-    const shadowRoot = document.createElement('div')
-    shadowRoot.id = 'improved-job-filter-root'
-    shadow.appendChild(shadowRoot)
-
-    const overlay = createApp(WebsiteConfig)
-    overlay.mount(shadowRoot)
-
-    const link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.href = cssUrl
-    shadow.appendChild(link)
-
+    createApp(WebsiteConfig).mount(host)
 }
 
 console.log('improved-job-filter loading')
