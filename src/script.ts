b408efbd9da29b2a3d313636d0cd3854bd61c2bc
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-01-13 01:06:35 +0000

    Found how to ignore site css while applying my own

--- src/script.ts
+++ src/script.ts
@@ -2,20 +2,33 @@ import { createApp } from 'vue'
 import WebsiteConfig from '@/components/WebsiteConfig.vue'
 import Filter from '@/utils/filter'
 
+import cssUrl from './assets/style.scss?url'
+import './assets/style.css'
+
 const id = 'improved-job-filter-overlay'
 
 function createOverlay(): void {
     if (document.querySelector(`#${id}`)) {
         return
     }
-    const overlayRoot = document.createElement('div')
-    overlayRoot.id = id
-    const body: HTMLBodyElement = document.querySelector('body') as HTMLBodyElement
+    const host = document.createElement('div')
+    host.id = id
+    document.body.appendChild(host)
 
-    body.appendChild(overlayRoot)
+    const shadow = host.attachShadow({ mode: "open" });
+
+    const shadowRoot = document.createElement('div')
+    shadowRoot.id = 'improved-job-filter-root'
+    shadow.appendChild(shadowRoot)
 
     const overlay = createApp(WebsiteConfig)
-    overlay.mount(`#${id}`)
+    overlay.mount(shadowRoot)
+
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = cssUrl
+    shadow.appendChild(link)
+
 }
 
 console.log('improved-job-filter loading')
